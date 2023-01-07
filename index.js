46dba23a048118015e34fd246f5e70a2ce6780a5
Steve Aldrin <stevealdrin.r2021@vitstudent.ac.in>
2023-01-07 19:39:42 +0530

    coffe cup added

---
+++ index.js
@@ -0,0 +1,13 @@
+document.querySelector(".coffe").addEventListener("click",function (){
+
+
+
+
+document.querySelector(".inside").classList.add("coffeanim");
+document.querySelector(".inside").style.transition="all 2s"
+
+
+document.querySelector(".smokes").style.visibility="visible";
+
+})
+jk
